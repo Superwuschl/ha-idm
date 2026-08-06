ae90b34dc954ed2a37af8a384e20cbb6e247db91
Bernd Weichhart <bernd.weichhart@t-online.de>
2026-08-06 08:47:38 +0200

    Refactor constants

--- custom_components/idm/const.py
+++ custom_components/idm/const.py
@@ -1,7 +1,25 @@
 """Constants for the iDM integration."""
 
+from datetime import timedelta
+
 DOMAIN = "idm"
 
-API_URL = "https://www.myidm.at"
+NAME = "iDM myIDM"
+
+MANUFACTURER = "iDM"
 
 DEFAULT_SCAN_INTERVAL = 60
+
+UPDATE_INTERVAL = timedelta(seconds=DEFAULT_SCAN_INTERVAL)
+
+API_URL = "https://myidm.at"
+
+PLATFORMS = [
+    "sensor",
+]
+
+CONF_INSTALLATION = "installation"
+
+DEVICE_IDENTIFIER = "main"
+
+ATTR_TEMPERATURE = "temperature"
