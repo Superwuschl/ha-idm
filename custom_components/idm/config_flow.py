c15a152d380a09b972e3770c5b9e16191166c3d5
Bernd Weichhart <bernd.weichhart@t-online.de>
2026-08-06 08:57:17 +0200

    Refactor constants

--- custom_components/idm/config_flow.py
+++ custom_components/idm/config_flow.py
@@ -1,5 +1,7 @@
 """Config flow for iDM integration."""
 
+from __future__ import annotations
+
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -13,12 +15,21 @@ class IDMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
-        """Handle user setup."""
+    async def async_step_user(
+        self,
+        user_input=None,
+    ):
+        """Handle the initial setup."""
 
         errors = {}
 
         if user_input is not None:
+            await self.async_set_unique_id(
+                f"idm_{user_input['installation']}"
+            )
+
+            self._abort_if_unique_id_configured()
+
             return self.async_create_entry(
                 title="iDM myIDM",
                 data=user_input,
@@ -26,8 +37,17 @@ class IDMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         schema = vol.Schema(
             {
-                vol.Required("username"): str,
-                vol.Required("password"): str,
+                vol.Required(
+                    "username"
+                ): str,
+
+                vol.Required(
+                    "password"
+                ): str,
+
+                vol.Required(
+                    "installation"
+                ): str,
             }
         )
 
