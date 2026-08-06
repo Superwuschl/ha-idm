f9c3894b6437bf97cb88525378ad98785b844896
Bernd Weichhart <bernd.weichhart@t-online.de>
2026-08-06 23:36:36 +0200

    clean available iDM diagrams

--- custom_components/idm/api.py
+++ custom_components/idm/api.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 import requests
 
-from homeassistant.core import HomeAssistant
-
 
 BASE_URL = "https://a.myidm.at"
 
@@ -12,13 +10,11 @@ class IDMApi:
 
     def __init__(
         self,
-        hass: HomeAssistant,
         access_token: str,
-        refresh_token: str,
+        refresh_token: str | None,
         wp_id: int,
     ):
 
-        self.hass = hass
         self.access_token = access_token
         self.refresh_token = refresh_token
         self.wp_id = wp_id
@@ -41,7 +37,7 @@ class IDMApi:
         }
 
 
-    def _get_sync(
+    def get(
         self,
         endpoint,
     ):
@@ -54,106 +50,35 @@ class IDMApi:
             timeout=20,
         )
 
-
-        if response.status_code == 401:
-            self.refresh()
-
-            response = requests.get(
-                url,
-                headers=self._headers(),
-                timeout=20,
-            )
-
-
         response.raise_for_status()
 
         return response.json()
 
 
 
-    async def get(
-        self,
-        endpoint,
-    ):
+    def heatpump(self):
 
-        return await self.hass.async_add_executor_job(
-            self._get_sync,
-            endpoint,
-        )
-
-
-
-    def refresh(self):
-
-        url = (
-            BASE_URL
-            + "/api/v1/oauth2/token/"
-        )
-
-
-        data = {
-            "refresh_token":
-                self.refresh_token,
-
-            "grant_type":
-                "refresh_token",
-        }
-
-
-        response = requests.post(
-            url,
-            data=data,
-            timeout=20,
-        )
-
-
-        response.raise_for_status()
-
-
-        token = response.json()
-
-
-        self.access_token = (
-            token["access_token"]
-        )
-
-
-        if "refresh_token" in token:
-            self.refresh_token = (
-                token["refresh_token"]
-            )
-
-
-        return token
-
-
-
-    async def heatpump(self):
-
-        return await self.get(
+        return self.get(
             f"/api/v1/heatpumps/{self.wp_id}/"
         )
 
 
+    def diagrams(self):
 
-    async def system_graph(self):
+        return self.get(
+            f"/api/v1/heatpumps/{self.wp_id}/diagrams/"
+        )
 
-        return await self.get(
+
+    def system_graph(self):
+
+        return self.get(
             f"/api/v1/heatpumps/{self.wp_id}/diagrams/graph_system/?period=24h"
         )
 
 
+    def heat_a_graph(self):
 
-    async def heat_a_graph(self):
-
-        return await self.get(
+        return self.get(
             f"/api/v1/heatpumps/{self.wp_id}/diagrams/graph_heat_a/?period=24h"
-        )
-
-
-
-    async def heat_b_graph(self):
-
-        return await self.get(
-            f"/api/v1/heatpumps/{self.wp_id}/diagrams/graph_heat_b/?period=24h"
         )
