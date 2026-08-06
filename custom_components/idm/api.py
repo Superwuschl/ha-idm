7d26b1e24668260d51002878b465e5e176a197de
Bernd Weichhart <bernd.weichhart@t-online.de>
2026-08-06 20:12:49 +0200

    Add heat circuit graph API methods

--- custom_components/idm/api.py
+++ custom_components/idm/api.py
@@ -25,13 +25,17 @@ class IDMApi:
         return {
             "Authorization":
                 f"Access-Token {self.access_token}",
+
             "Accept":
                 "application/json, text/plain, */*",
+
             "Origin":
                 "https://app.myidm.at",
+
             "Referer":
                 "https://app.myidm.at/",
         }
+
 
 
     def refresh(self):
@@ -45,6 +49,7 @@ class IDMApi:
         data = {
             "refresh_token":
                 self.refresh_token,
+
             "grant_type":
                 "refresh_token",
         }
@@ -69,6 +74,7 @@ class IDMApi:
 
 
         if "refresh_token" in token:
+
             self.refresh_token = (
                 token["refresh_token"]
             )
@@ -78,7 +84,10 @@ class IDMApi:
 
 
 
-    def get(self, endpoint):
+    def get(
+        self,
+        endpoint,
+    ):
 
         url = BASE_URL + endpoint
 
@@ -94,6 +103,7 @@ class IDMApi:
 
             self.refresh()
 
+
             response = requests.get(
                 url,
                 headers=self._headers(),
@@ -102,6 +112,7 @@ class IDMApi:
 
 
         response.raise_for_status()
+
 
         return response.json()
 
@@ -127,4 +138,12 @@ class IDMApi:
 
         return self.get(
             f"/api/v1/heatpumps/{self.wp_id}/diagrams/graph_heat_a/?period=24h"
+        )
+
+
+
+    def heat_b_graph(self):
+
+        return self.get(
+            f"/api/v1/heatpumps/{self.wp_id}/diagrams/graph_heat_b/?period=24h"
         )
